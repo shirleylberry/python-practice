3a0ba01280289f05fe2695776e91603cace58081
shirleylberry <shirleylberry@gmail.com>
2015-09-23 18:05:20 -0400

    continuing unit 4

--- udacity/slb-cookies/main.py
+++ udacity/slb-cookies/main.py
@@ -10,17 +10,15 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 def hash_str(s):
-    return hashlib.md5(s)
+    return hashlib.md5(s).hexdigest()
 
 def make_secure_val(s):
-    return "%s,%s" %(s, hash_str(s))
+    return "%s|%s" %(s, hash_str(s))
 
 def check_secure_val(h):
-    orig_word = h.split(',')
-    if orig_word[1] == hash_str(h[0]):
-        return h[0]
-    else:
-        return None
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -38,18 +36,21 @@ class MainPage(Handler):
         self.response.headers['Content-Type'] = 'text/plain'
         visits = 0
         visit_cookie_str = self.request.cookies.get('visits')
-        if visit_cookie_val:
+        if visit_cookie_str:
             cookie_val = check_secure_val(visit_cookie_str)
             if cookie_val:
                 visits = int(cookie_val)
+
         visits += 1
 
         new_cookie_val = make_secure_val(str(visits))
-        self.response.headers.add_header('Set-Cookie', 'visits=%s' %visits, [])
+
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+
         if visits > 100:
             self.write("You are the best ever.")
         else:
-            self.write("you've been here %s times" % str(visits))
+            self.write("you've been here %s times" % visits)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
